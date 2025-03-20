d9a8ba4729223682ddc5a022d63f4ddc8b5aead2
RangoAx <alanabichayn3@gmail.com>
2025-03-20 12:48:55 -0300

    Agregado maquina de estado y jugador
    jajjjajaaj xd

---
+++ escenas/componentes/maquina_estado/estado.gd
@@ -0,0 +1,16 @@
+extends Node
+class_name Estado
+
+var jugador : CharacterBody2D
+
+func _ready():
+	jugador = get_parent().jugador
+
+func enter():
+	pass
+	
+func update(_delta):
+	pass
+
+func exit():
+	pass
---
+++ escenas/componentes/maquina_estado/maquina_estado.gd
@@ -0,0 +1,17 @@
+extends Node
+class_name MaquinaEstado
+
+@export var jugador : CharacterBody2D
+var estado_actual : Estado
+
+func _ready():
+	estado_actual = get_child(0)
+	estado_actual.enter()
+	
+func _process(delta):
+	estado_actual.update(delta)
+
+func ir_a_estado_siguiente(estado_siguiente : Estado):
+	estado_actual.exit()
+	estado_actual = estado_siguiente
+	estado_actual.enter()
